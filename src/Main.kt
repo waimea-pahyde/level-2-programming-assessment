d237b4e8c65bfece3b870e2e389f627e08b5fe00
polly washere <pahyde@waimea.school.nz>
2025-03-18 14:47:04 +1300

    Project Setup

--- src/Main.kt
+++ src/Main.kt
@@ -2,12 +2,13 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Name:   OLD GOLD
+ * Project Author: POLLY HYDE
+ * GitHub Repo:    https://github.com/waimea-pahyde/level-2-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
- * PROJECT NOTES HERE
+ * This is a two-player game, played on a one-dimensional grid with coins, where the aim is to win by being the player who removes the gold coin.
+ *
  * =====================================================================
  */
 
